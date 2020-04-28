806e56d067f17120f0045d12c61c59b8f2a12b8a
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-04-27 20:45:46 -0400

    beat box commit with api call

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios"
+
 
 function App() {
 
+  const [track, updateTrack] = useState([])
+
+  useEffect(() => {
+
+    apiCall()
+
+  }, [])
+
+
+  const apiCall = async () => {
+    const response = await axios({
+      url: "http://ws.audioscrobbler.com/2.0/?method=track.search&api_key=9357323b21f3ac3a16289e7e62479e88&format=json",
+      params: {
+        track: "Believe"
+      }
+    })
+    console.log(response)
+  }
+  // updateTrack(response.results.track.name)
 
 
 
 
 
   return (
+
     <div className="App">
       <header className="App-header">
         <nav>
@@ -18,14 +40,14 @@ function App() {
       <main>
         <form className="djname">
           <input type="text" placeholder="Create Your Own DJ Name"></input>
-          <button type="button">Create</button>
+          {/* <button type="button">Create</button> ... Enter will be the on Submit ... will just the name show up in my particular font/size and box will disappear? */}
         </form>
         <form className="playlist">
-          <input type="text" placeholder="Artist/Song Search"></input>
+          <input type="text" placeholder="Track Search"></input>
           <button type="button">+ Add Song</button>
         </form>
         <div>
-          <p> Playlist here </p>
+          <p> Search Results Here </p>
 
 
         </div>
@@ -45,3 +67,5 @@ export default App;
 //have onSubmit attach to a form for input and button
 
 // handleChange stores what is in the input box
+
+//Enter be onSubmit for Create a DJ name
--- src/index.js
+++ src/index.js
@@ -15,3 +15,21 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import {
+//   BrowserRouter as Router
+// } from "react-router-dom"
+// import App from './App';
+
+
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>,
+//   document.getElementById('root')
+// );
+
